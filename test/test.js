eab0b6e0b2bcd9024eecdb91a4d83e13ad53735d
Ling Ling Chang <ling@lightning-bolt.com>
2016-03-12 14:55:06 -0800

    add sample mocha chai test case

---
+++ test/test.js
@@ -0,0 +1,37 @@
+var should = require('chai').should(),
+expect = require('chai').expect,
+supertest = require('supertest'),
+api = supertest('http://restcountries.eu/rest/v1');
+
+var $data = api.get('/name/norway')
+
+describe('Check Headers',function(){
+	it('should return a 200 response', function(done) {
+		api
+		.get('/name/norway')
+		.expect(200,done);
+	});
+});
+
+describe('Nprway',function(){	
+	it('has capital "Oslo"', function(done){
+		api
+			.get('/name/norway')
+			.expect(200)
+			.end(function(err,res) {
+				expect(res.body[0]).to.have.property("capital");
+				expect(res.body[0].capital).to.equal("Oslo");
+				//console.log(res.body[0]);
+
+				done();
+		});
+	});
+});
+
+describe('asdf', function(){
+	it('asdf', function(done){zxzdfe      
+		$data.expect(200);
+		done();
+	})
+})
+
